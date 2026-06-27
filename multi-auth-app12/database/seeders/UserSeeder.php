6214248326848966f993fd32d1103994192e8f93
iftykharliyes999-oss <iftykharliyes999@gmail.com>
2026-06-27 13:11:13 +0600

    laravel

---
+++ multi-auth-app12/database/seeders/UserSeeder.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::create([
+            'name' => 'Admin',
+            'email' => 'admin@gmail.com',
+            'password' => Hash::make('123456'),
+            'role' => 'admin'
+        ]);
+
+        User::create([
+            'name' => 'Teacher',
+            'email' => 'teacher@gmail.com',
+            'password' => Hash::make('123456'),
+            'role' => 'teacher'
+        ]);
+
+        User::create([
+            'name' => 'Student',
+            'email' => 'student@gmail.com',
+            'password' => Hash::make('123456'),
+            'role' => 'student'
+        ]);
+    }
+}
